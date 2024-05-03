59a9a89fd43142bc750e08b41a5d2eaefc52c24e
DEMON-AND-MAX <demon.and.max@gmail.com>
2024-05-03 13:00:45 +0300

    Add extension ignore and impose controls

--- Asset Glossary/scene/level_resource.gd
+++
@@ -1,7 +0,0 @@
-class_name LevelResource
-extends Resource
-
-@export var name: String
-
-@export var song_list: Array[AudioStream]
-
--- Asset Glossary/script/asset_glossary.gd
+++ Asset Glossary/script/asset_glossary.gd
@@ -3,25 +3,24 @@ extends Runnable
 
 @export var asset_dir_path: String = "res://asset/music/"
 
-#@export var ignore_extension_list: Array[String] = [
-	#"import",
-#]
-#@export var unoptimal_extension_list: Array[String] = [
-	#"wav", "mp3"
-#]
+@export_category("Extension Settings")
+@export var ignore_extension_list: Array[String] = [
+	"import",
+]
+@export var b_is_ignore_impose: bool = true
+@export var unoptimal_extension_list: Array[String] = [
+	"wav", "mp3"
+]
+@export var b_is_unoptimal_impose: bool = false
 
 @export_category("Directory Depth Settings")
 @export_range(0, 8) var max_dir_depth: int = 2
-@export var b_is_depth_imposed: bool = false
-@export var b_is_file_depth_max: bool = true
+@export var b_is_depth_impose: bool = false
+@export var b_is_file_depth_max: bool = false
 @export var b_is_file_dir_exclusive: bool = true
-
-#@export var b_load_unoptimal: bool = true
 
 @export_category("Warnings")
 @export var b_is_print_warning: bool = true
-
-#var asset_glossary: Dictionary
 
 func run() -> void:
 	_parse_glossary()
@@ -39,73 +38,59 @@ func _parse_folder(dir_path: String, dir_depth: int = 0) -> void:
 	
 	condition = dir_depth > max_dir_depth
 	message = ("Directory depth over maximum (" + str(max_dir_depth) +
-	"), with value " + str(dir_depth) + ". Folder path: " + dir_path)
+	"), with value " + str(dir_depth) + ".\n" + 
+	"Directory path: " + dir_path)
 	
-	_issue(condition, message, b_is_depth_imposed, b_is_print_warning)
+	_issue(condition, message, b_is_depth_impose, b_is_print_warning)
 	
 	dir.list_dir_begin()
 	var file = dir.get_next()
+	var b_dir: bool = false
 	
 	while file != "":
 		var file_path = dir.get_current_dir() + "/" + file
 		if dir.current_is_dir():
 			_parse_folder(file_path, dir_depth + 1)
+			b_dir = true
 		else:
-			condition = b_is_file_depth_max and dir_depth < max_dir_depth
-			message = ("File depth under maximum (" + str(max_dir_depth) +
-			"), with value " + str(dir_depth) + ". File path: " + 
-			dir_path + "/" + file)
+			if b_is_ignore_impose and file.get_extension() in ignore_extension_list:
+				break
 			
-			_issue(condition, message, b_is_depth_imposed, b_is_print_warning)
+			condition = b_is_file_dir_exclusive and b_dir
+			message = ("Directory already occupied by subdirectories, " +
+			"move file destination to maintain hierarchy.\n" +
+			"File path: " + file_path)
 			
-			# todo check file extension
+			_issue(condition, message, b_is_depth_impose, b_is_print_warning)
 			
+			_parse_file(file_path, file, dir_depth)
 		file = dir.get_next()
 	
 	dir.list_dir_end()
 
-func _issue(b_condition: bool, message: String, 
-		b_is_assert: bool = false, b_is_print_warning: bool = true) -> void:
-	if b_condition:
-		assert(!b_is_assert, message)
-		
-		if b_is_print_warning:
-			print("WARNING: " + message)
+func _parse_file(dir_path: String, file: String, dir_depth: int) -> void:
+	var condition: bool
+	var message: String
+	
+	condition = b_is_file_depth_max and dir_depth < max_dir_depth
+	message = ("File depth under maximum (" + str(max_dir_depth) +
+	"), with value " + str(dir_depth) + ".\n" +
+	"File path: " + dir_path)
+	
+	_issue(condition, message, b_is_depth_impose, b_is_print_warning)
+	
+	condition = b_is_unoptimal_impose and \
+			file.get_extension() in unoptimal_extension_list
+	message = ("File type unoptimal (" + file.get_extension() +
+	"), convert to a more suitable file type.\n" +
+	"File path: " + dir_path)
+	
+	_issue(condition, message, b_is_unoptimal_impose, b_is_print_warning)
 
-#func _add_to_glossary(file_path: String, file_extension: String = "") -> void:
-	#if file_extension in ignore_extension_list:
-		#return
-	#
-	#var file_path_split = file_path.split("/")
-	#var file_name = file_path_split[-1].split(".")[0]
-	#file_extension = file_path_split[-1].split(".")[1]
-	#
-	#if file_extension in unoptimal_extension_list:
-		#if b_print_warnings:
-			#print("WARNING: " + file_path_split[-1] + 
-			#" is of an unoptimal file type, consider converting " +
-			#"to a different type!")
-		#if !b_load_unoptimal:
-			#return
-	#
-	#if file_extension not in asset_glossary.keys():
-		#asset_glossary[file_extension] = Dictionary()
-	#
-	#var current_dict = asset_glossary[file_extension]
-	#
-	#if file_path_split[3] not in current_dict.keys():
-		#current_dict[file_path_split[3]] = Dictionary()
-	#
-	#current_dict = current_dict[file_path_split[3]]
-	#
-	#if file_name.contains(file_path_split[-2]):
-		#if file_path_split[-2] not in current_dict.keys():
-			#current_dict[file_path_split[-2]] = Dictionary()
-		#
-		#current_dict = current_dict[file_path_split[-2]]
-	#
-	## remove parent folder name from the child
-	#if file_name.find(file_path_split[-2]) != -1:
-		#file_name = file_name.erase(file_name.find(file_path_split[-2]), file_path_split[-2].length() + 1)
-	#
-	#current_dict[file_name] = file_path
+func _issue(b_condition: bool, message: String, 
+		b_assert: bool = false, b_warning: bool = true) -> void:
+	if b_condition:
+		assert(!b_assert, message)
+		
+		if b_warning:
+			print("WARNING: " + message)
